d0ef4671d5f82f37a9fa1692c74ecc6181d39050
akella <akella.a@gmail.com>
2015-02-23 18:24:14 +0200

    fix scroll

--- js/common.js
+++ js/common.js
@@ -57,14 +57,19 @@ head.ready(function() {
         });
 
         function scrollToparea(direction) {
+            disable_scroll();
             if ( direction == 'top' ) {
                 body.animate({
                     scrollTop: 0
-                }, 800);
+                }, 800, function(){
+                      enable_scroll();
+                });
             } else {
                 body.animate({
                     scrollTop: topareaHeight
-                }, 800);
+                }, 800, function(){
+                      enable_scroll();
+                });
             }
             toggleAbilityScrollToparea(800);
         }
@@ -728,6 +733,45 @@ head.ready(function() {
         });
     })();
 
+
+    // left: 37, up: 38, right: 39, down: 40,
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    var keys = [37, 38, 39, 40];
+
+    function preventDefault(e) {
+      e = e || window.event;
+      if (e.preventDefault)
+          e.preventDefault();
+      e.returnValue = false;  
+    }
+
+    function keydown(e) {
+        for (var i = keys.length; i--;) {
+            if (e.keyCode === keys[i]) {
+                preventDefault(e);
+                return;
+            }
+        }
+    }
+
+    function wheel(e) {
+      preventDefault(e);
+    }
+
+    function disable_scroll() {
+      if (window.addEventListener) {
+          window.addEventListener('DOMMouseScroll', wheel, false);
+      }
+      window.onmousewheel = document.onmousewheel = wheel;
+      document.onkeydown = keydown;
+    }
+
+    function enable_scroll() {
+        if (window.removeEventListener) {
+            window.removeEventListener('DOMMouseScroll', wheel, false);
+        }
+        window.onmousewheel = document.onmousewheel = document.onkeydown = null;  
+    }
 
 });
     // var path = document.querySelector('#main-line');
